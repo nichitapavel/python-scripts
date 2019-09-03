63972495f046c27edaf95763419a7af1a36c676d
Pavel Nichita <pnichita@ull.edu.es>
2019-09-03 15:41:17 +0100

    -DRAFT tweaks for previous data files

--- metrics_log_process.py
+++ metrics_log_process.py
@@ -23,20 +23,21 @@ def metrics_file_process(cwd: str, file: str) -> [dict]:
     logger.info(f'[{cwd}][{file}]')
     data = []
     name_parsed = csv_name_parsing(file)
+    name_parsed['size'] = None
     with open(file, 'r') as f:
         lines = f.readlines()
         i = 0
         while i < len(lines):
             if 'Run:' in lines[i]:
-                i, metric_dict = block_process(i, lines, name_parsed)
-                data.append(metric_dict)
+                i, metric_dict = block_process_run(i, lines, name_parsed)
+                data.extend(metric_dict)
             i += 1
     # mark this file as processed
     open(f'read-{file}', 'w').close()
     return data
 
 
-def block_process(i: int, lines: list, name_parsed: dict) -> 'int,dict':
+def block_process_run(i: int, lines: list, name_parsed: dict) -> 'int,dict':
     """
     Process data between lines with 'Run: n' and 'Run: n+1'.
     This function has a sketchy implementation and philosophy in general,
@@ -46,17 +47,41 @@ def block_process(i: int, lines: list, name_parsed: dict) -> 'int,dict':
     :param name_parsed: a dict with key and value predefined
     :return: int i: position for previous line before 'Run: n+1', dict
     """
-    metric_dict = name_parsed.copy()
     iteration = int(lines[i].split()[2])
+    iter_dict = name_parsed.copy()
+    iter_dict.update({'iteration': f'{iteration:03}'})
+    # Let go to the next line, it should have 'Size: x'
+    i += 1
+    dicts = []
+    # try/except block for when we get to the appearance of last 'Run: x'
+    # since after this block there will be no more of 'Run: x' it will raise
+    # an IndexError. No more available lines to process.
+    try:
+        while 'Run:' not in lines[i]:
+            i, ret_dicts = block_process_size(i, lines, iter_dict)
+            dicts.append(ret_dicts)
+            i += 1
+            # While in 'Run: n' block we must go to the next line
+        # We are at the line with 'Run: n+1', we must set the position to the previous
+        # line so that outer function can read it and launch this function again
+        i -= 1
+    except IndexError:
+        pass
+    return i, dicts
+
+
+def block_process_size(i: int, lines: list, name_parsed: dict) -> 'int,dict':
+    metric_dict = name_parsed.copy()
     time_npb = ''
     mops = ''
+    size = lines[i].split()[2].lower()  # In case there is no npb data
     # Let go to the next line, it should have 'Size: x'
     i += 1
     # try/except block for when we get to the appearance of last 'Run: x'
     # since after this block there will be no more of 'Run: x' it will raise
     # an IndexError. No more available lines to process.
     try:
-        while 'Run:' not in lines[i]:
+        while 'Run' not in lines[i] and 'Size:' not in lines[i]:
             if 'Class' in lines[i]:
                 metric_dict['size'] = lines[i].split()[3].lower()
             if 'Time in ' in lines[i]:
@@ -65,12 +90,15 @@ def block_process(i: int, lines: list, name_parsed: dict) -> 'int,dict':
                 mops = float(lines[i].split()[4])
             # While in 'Run: n' block we must go to the next line
             i += 1
+        # Check if there is no npb data
+        if metric_dict['size'] is None:
+            metric_dict['size'] = size
         # We are at the line with 'Run: n+1', we must set the position to the previous
         # line so that outer function can read it and launch this function again
         i -= 1
     except IndexError:
         pass
-    metric_dict.update({'iteration': f'{iteration:03}', 'time_npb': time_npb, 'mops': mops})
+    metric_dict.update({'time_npb': time_npb, 'mops': mops})
     return i, metric_dict
 
 
@@ -106,7 +134,7 @@ def main():
             processed_data.extend(result.get())
 
     sort_list_of_dict(processed_data)
-    profile(mem, 'main', write_csv_list_of_dict, 'metrics_data.csv', processed_data, logger)
+    profile(mem, 'main', write_csv_list_of_dict, 'metrics_data.csv', processed_data, logger, True)
 
 
 if __name__ == "__main__":
---
+++ stats.py
@@ -0,0 +1,230 @@
+import logging
+import os
+import sys
+from optparse import OptionParser
+
+import matplotlib.pyplot as plt
+import numpy
+import pandas as pd
+import seaborn as sns
+
+from common import parse_args, log_to_file, IDs, profile, DataFilterItems, ResultItems, write_csv_list_of_dict
+from plotters import box_plot
+
+
+def parse_args(logger):
+    # Parsear linea de comandos
+    parser = OptionParser("usage: %prog -d|--directory DIRECTORY")
+    parser.add_option("--df", "--data-file", action="store", type="string", dest="data_file")
+    parser.add_option("--sd", "--save-directory", action="store", type="string", dest="save_directory")
+    (options, args) = parser.parse_args()
+    if not options.data_file or \
+            not options.save_directory:
+        # This logger line will not be saved to file
+        logger.error('[You must specify files and a directory to save]')
+        parser.print_help()
+        sys.exit(-1)
+    return options
+
+
+def clean_data(df):
+    """
+    Clean data from Null (empty) or 0.0 values
+    :param df: DataFrame read with panda from a csv (usually and in my case)
+    :return: DataFrame without Null (empty) or 0.0 values
+    """
+    df = df.dropna()
+    # df = df[df[IDs.ITERATION] > 10]
+    for item in ResultItems:
+        df = df[df[item] != 0]
+    return df
+
+
+def set_data_labels(cp):
+    bars = cp.ax.patches
+    for bar in bars:
+        bar_width = bar.get_width()
+        text_x = bar_width / 2 + bar.get_x()
+        bar_height = bar.get_height()
+        if not numpy.isnan(bar_height):
+            text_y = bar_height + bar_height * 0.01
+            cp.ax.text(x=text_x, y=text_y, s=f'{bar_height:.2f}', ha='center', fontsize=9)
+
+
+def cat_plotting(cwd, df, options, x_axis_groupby, type):
+    df_groups = df.groupby(list(set(DataFilterItems) - set([x_axis_groupby])))
+    values = ['mean', 'q2_median']
+    for value in values:
+        for group in df_groups:
+            name = f'catplot_{value}_{type}_{x_axis_groupby}_{"_".join(str(x) for x in group[0])}'
+            cp = sns.catplot(x=IDs.THREADS, y=value, data=group[1], height=6, kind="bar", palette="muted")
+            set_data_labels(cp)
+            cp.savefig(name)
+            cp.fig.clf()
+            plt.close()
+            logger.info(f'[{options.data_file}][CATPLOT][{name}]')
+
+
+def cat_plotting_group(cwd, df, options, x_axis_groupby, type):
+    df_groups = df.groupby(list(set(DataFilterItems) - set(x_axis_groupby)))
+    values = ['mean', 'q2_median']
+    for value in values:
+        for group in df_groups:
+            name = f'catplot_{value}_{type}_{"_".join(x_axis_groupby)}_{"_".join(str(x) for x in group[0])}'
+            title = f'{type}_{"_".join(group[0])}'
+            cp = sns.catplot(x=IDs.THREADS, y=value, hue=x_axis_groupby[1], data=group[1], height=6, kind="bar", palette="muted")
+            set_data_labels(cp)
+            cp.ax.set_title(title)
+            cp.savefig(name)
+            cp.fig.clf()
+            plt.close()
+            logger.info(f'[{options.data_file}][CATPLOT][HUE][{name}]')
+
+
+def box_plotting(cwd, df, options, x_axis_groupby):
+    """
+    Creates boxplots with data from :df and saves them in :cwd.
+    :df has type,device,os,benchmark and threads, using :x_axis_groupby
+    we group data by excluding :x_axis_groupby, and after we use it
+    for the x axis.
+    :param cwd: string, full path to current working directory
+    :param df: pandas DataFrame, data read from a csv file (usually and in my case)
+    :param options: optparse.Values object, parsed arguments, used for logging
+    :param x_axis_groupby: string, the x axis of the boxplot
+    :return:
+    """
+    df_groups = df.groupby(list(set(DataFilterItems) - set([x_axis_groupby])))
+    for group in df_groups:
+        unique = group[1][x_axis_groupby].drop_duplicates()
+        dicts = {}
+        for i in ResultItems:
+            dicts[i] = {}
+            for j in unique:
+                dicts[i][j] = group[1][group[1][x_axis_groupby] == j][i].values.tolist()
+        for keys, values in dicts.items():
+            name = f'{x_axis_groupby}_{"_".join(str(x) for x in group[0])}'
+            box_plot(cwd, name, keys, values)
+            logger.info(f'[{options.data_file}][BOXPLOT][{name}]')
+            plt.close()
+
+
+def groups_plotting(name, data, options, x_group, x_axis):
+    name_time = f'{name}_{IDs.TIME}_{x_axis}_{x_group}.png'
+    logger.info(f'[{options.data_file}][BOXPLOT][{name_time}]')
+    bp = sns.boxplot(x=x_axis, y=IDs.TIME, hue=x_group, data=data)
+    bp.get_figure().savefig(name_time)
+    bp.get_figure().clf()
+    name_energy = f'{name}_{IDs.ENERGY}_{x_axis}_{x_group}.png'
+    logger.info(f'[{options.data_file}][BOXPLOT][{name_energy}]')
+    bp = sns.boxplot(x=x_axis, y=IDs.ENERGY, hue=x_group, data=data)
+    bp.get_figure().savefig(name_energy)
+    bp.get_figure().clf()
+    name_mops = f'{name}_{IDs.MOPS}_{x_axis}_{x_group}.png'
+    logger.info(f'[{options.data_file}][BOXPLOT][{name_mops}]')
+    bp = sns.boxplot(x=x_axis, y=IDs.MOPS, hue=x_group, data=data)
+    bp.get_figure().savefig(name_mops)
+    bp.get_figure().clf()
+
+
+def box_plotting_groups(cwd, df, options, x_axis_groupby):
+    df_groups = df.groupby(list(set(DataFilterItems) - set(x_axis_groupby)))
+    for group in df_groups:
+        name = f'{"_".join(str(x) for x in group[0])}'
+        groups_plotting(name, group[1], options, x_axis_groupby[0], x_axis_groupby[1])
+        groups_plotting(name, group[1], options, x_axis_groupby[1], x_axis_groupby[0])
+
+
+def update_dict(stats, data_dict, type_of_data):
+    new_dict = data_dict.copy()
+    for index in stats.index:
+        new_dict.update({
+            index: stats[type_of_data][index]
+        })
+    return new_dict
+
+
+def create_and_write_stats(df):
+    df_groups = df.groupby(DataFilterItems)
+    stats_results = {IDs.TIME: [], IDs.TIME_NPB: [], IDs.ENERGY: [], IDs.MOPS: []}
+    for group in df_groups:
+        data_dict = {
+            IDs.TYPE: group[0][0],
+            IDs.DEVICE: group[0][1],
+            IDs.OS: group[0][2],
+            IDs.BENCH: group[0][3],
+            IDs.THREADS: group[0][4].item()
+        }
+        stats = group[1][ResultItems].agg(['count', 'mean', 'std', 'var', 'mad', 'min', 'max'])
+        stats = stats.append(group[1][ResultItems]
+                             .quantile([0.25, 0.5, 0.75])
+                             .rename(index={0.25: 'q1', 0.5: 'q2_median', 0.75: 'q3'})
+                             )
+        for result in ResultItems:
+            stats_dict = update_dict(stats, data_dict, result)
+            stats_results[result].append(stats_dict)
+    for result in ResultItems:
+        write_csv_list_of_dict(f'stats_{result}.csv', stats_results[result], logger, overwrite=True)
+
+    return stats_results
+
+
+def main():
+    options = parse_args(logger)
+    os.chdir(options.save_directory)
+    logger.addHandler(log_to_file('stats.log'))
+    cwd = os.getcwd()
+
+    df = pd.read_csv(options.data_file)
+    df = clean_data(df)
+
+    # box_plotting(cwd, df, options, x_axis_groupby=IDs.THREADS)
+    # box_plotting(cwd, df, options, x_axis_groupby=IDs.OS)
+    # box_plotting(cwd, df, options, x_axis_groupby=IDs.DEVICE)
+    # box_plotting(cwd, df, options, x_axis_groupby=IDs.TYPE)
+    # logger.info(f'[{options.data_file}]')
+    #
+    # box_plotting_groups(cwd, df, options, [IDs.DEVICE, IDs.THREADS])
+    # box_plotting_groups(cwd, df, options, [IDs.DEVICE, IDs.BENCH])
+    # box_plotting_groups(cwd, df, options, [IDs.DEVICE, IDs.TYPE])
+    # box_plotting_groups(cwd, df, options, [IDs.DEVICE, IDs.OS])
+    #
+    # box_plotting_groups(cwd, df, options, [IDs.THREADS, IDs.BENCH])
+    # box_plotting_groups(cwd, df, options, [IDs.THREADS, IDs.TYPE])
+    # box_plotting_groups(cwd, df, options, [IDs.THREADS, IDs.OS])
+    #
+    # box_plotting_groups(cwd, df, options, [IDs.BENCH, IDs.TYPE])
+    # box_plotting_groups(cwd, df, options, [IDs.BENCH, IDs.OS])
+    #
+    # box_plotting_groups(cwd, df, options, [IDs.TYPE, IDs.OS])
+
+    stats = create_and_write_stats(df)
+    # for resultItem in ResultItems:
+    #     pd_stats = pd.DataFrame(stats[resultItem])
+    #     cat_plotting(cwd, pd_stats, options, IDs.THREADS, resultItem)
+    #     cat_plotting_group(cwd, pd_stats, options, [IDs.THREADS, IDs.OS], resultItem)
+    #     cat_plotting_group(cwd, pd_stats, options, [IDs.THREADS, IDs.TYPE], resultItem)
+
+    # df = sns.load_dataset('tips')
+    # sns.boxplot(x = "day", y = "total_bill", hue = "smoker", data = df, palette = "Set1")
+
+
+if __name__ == "__main__":
+    logger = logging.getLogger('STATS_CSV')
+    pd.options.display.width = 0
+    mem = []
+    profile(mem, 'test', main,)
+
+    mem.sort()
+    for item in mem:
+        print(item)
+
+
+# PANDA
+# df_sub = df[ df['salary'] > 120000 ] -> select data where salary > 120 000
+# data frame method: mean, median, mod, mad, max, min, std (standard deviation), var, dropna (drop records with no values)
+# object.describe -> does many of previous
+# df_rank = df.groupby(['rank']) -> split data based on rank
+# df.groupby(['rank'], sort=False) -> sort=False for speedup
+# df_sorted = df.sort_values( by ='service') -> sort by value of column service
+# flights[flights.isnull().any(axis=1)] -> Select the rows that have at least one missing value
+# flights[['dep_delay','arr_delay']].agg(['min','mean','max']) -> compute multiple statistics at once
